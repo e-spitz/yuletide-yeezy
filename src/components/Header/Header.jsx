7e9d87a9ce7fb706ff354de743a59d7e8c9997be
Erica <ericaspitz522@gmail.com>
2021-09-24 08:04:40 -0600

    Add png image on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ export const Header = () => {
     <header className="header">
       <div className='header-div'>
         <p className='header-title'>yuletide yeezy</p>
+        <img className='png' src={require('../../assets/Kanye-West.png').default}/>
         <p className='header-des'>...for the jolly & jovial Kanye fan</p>
       </div>
     </header>
