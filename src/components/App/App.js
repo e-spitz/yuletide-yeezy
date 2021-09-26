dcd486876c394f4f5e134f08debe788a1a92f4cd
Erica <ericaspitz522@gmail.com>
2021-09-26 08:52:17 -0600

    Add error component with navlink to error image

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { Header } from '../Header/Header'
 import { GenerateSection } from '../GenerateSection/GenerateSection'
 import { Favorites } from '../Favorites/Favorites'
+import { Error } from '../Error/Error'
 import { Route, Switch, NavLink } from 'react-router-dom';
 
 function App() {
@@ -12,18 +13,13 @@ function App() {
     setFavs([favObj, ...favs])
   }
 
-  // const removeFavorite = (id) => {
-  //   const remainingCards = setFavs.filter(fav => id !== fav.id)
-  //   console.log(remainingCards)
-  //   setFavs(remainingCards)
-  // }
-
   return (
     <div className="App">
       <Header />
       <Switch>
         <Route exact path='/yuletide-yeezy' render={() => <GenerateSection addFavorite={addFavorite} favorites={favs}/>}/>
         <Route exact path='/favorites' render={() => <Favorites />}/>
+        <Route exact path='*' render={() => <Error />}/>
       </Switch>
     </div>
   );
