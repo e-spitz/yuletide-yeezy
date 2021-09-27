1e9cdbfbbbc09e1a293039e7d833264464802ba3
Erica <ericaspitz522@gmail.com>
2021-09-27 14:43:25 -0600

    Clean up app and add prop types

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 import { useState } from 'react';
-import { Header } from '../Header/Header'
 import { GenerateSection } from '../GenerateSection/GenerateSection'
 import { Favorites } from '../Favorites/Favorites'
 import { Error } from '../Error/Error'
 import { Login } from '../Login/Login'
-import { Route, Switch, NavLink } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 function App() {
   const [favs, setFavs] = useState([])
-  const [noClicked, setNoClicked] = useState(false)
-  const [yesClicked, setYesClicked] = useState(false)
 
   const addFavorite = (favObj) => {
     setFavs([favObj, ...favs])
@@ -29,3 +27,8 @@ function App() {
 }
 
 export default App;
+
+App.propTypes = {
+  favs: PropTypes.array,
+  addFavorite: PropTypes.func
+}
