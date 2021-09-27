1b98160e4d22747ccf89797fc93fa2319ff09dd1
Erica <ericaspitz522@gmail.com>
2021-09-26 18:35:09 -0600

    Fix tests to pass

--- cypress/integration/Favorites_spec.js
+++ cypress/integration/Favorites_spec.js
@@ -24,13 +24,12 @@ describe('Favorites Page', () => {
     cy.visit('http://localhost:3000/yuletide-yeezy')
     .get('.gen-image-btn').click()
     .get('.gen-quote-btn').click()
-    .wait(2000)
+    cy.wait(2000)
     .get('.save-to-fav-btn').click()
     cy.get('.view-fav-btn').click()
     cy.url().should('eq', 'http://localhost:3000/favorites')
     cy.wait(2000)
-    .get('.card-container').get('.fav-card').get('.card-quote').get('p').get('.remove-btn')
+    cy.get('.card-container').get('.fav-card').get('.card-quote').get('p').get('.remove-btn')
   });
-
 
 })
